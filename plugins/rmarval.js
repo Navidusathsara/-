388c894f7991a6a0d18ccaa23143e59a4cf78674
Navidusathsara <86136949+Navidusathsara@users.noreply.github.com>
2021-06-25 16:07:27 +0530

    Update rmarval.js

--- plugins/rmarval.js
+++ plugins/rmarval.js
@@ -31,7 +31,7 @@ else if (Config.WORKTYPE == 'public') {
 
         if (match[1] === '') return await message.sendMessage(need);
 
-        var ttinullimage = await axios.get(`https://api.zeks.xyz/api/marvellogo?apikey=cjxJIn9eDletRHac3CG6CdvGxFH&text1=${encodeURIComponent(match[1])}&text2=STUDIO`, { responseType: 'arraybuffer' })
+        var ttinullimage = await axios.get(`https://api.zeks.xyz/api/wolflogo?apikey=00oQhHnqcqFGofHbjsFtXZcKVWO&text1=ZEKS&text2=API=${encodeURIComponent(match[1])}&text2=STUDIO`, { responseType: 'arraybuffer' })
 
         await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'RAVANA💢' })
 
